ba3ac724178221ad427b99dfecc10d9e6fc8a2a1
SeyedAmir-Salimi <you@example.com>
2020-08-15 17:22:07 +0200

    rendering fix for melds

--- src/components/Meld.js
+++ src/components/Meld.js
@@ -1,49 +1,31 @@
+/* eslint-disable react/display-name */
 import "./Meld.css";
 
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
-import { IoMdCheckmarkCircleOutline } from "react-icons/io";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
 import cardImages from "../assets/cards.json";
-import { toggle_selected_meld } from "../redux/actions/actions";
+import MeldSelection from "./MeldSelection";
 
-function Meld({ card }) {
-  const [isSelected, setIsSelected] = useState(false);
-  const dispatch = useDispatch();
-
+function areEqual(prevProps, nextProps) {
+  return prevProps.cardId === nextProps.cardId;
+}
+const Meld = React.memo(({ card }) => {
+  console.log("Meld");
   const cardObject = cardImages.filter((x) => x.id === card.cardId)[0];
   const imageFile = require(`../assets/images/${cardObject.image}`);
-  const selectedMeld = useSelector((state) => state.gameInfo.selectedMeld);
 
   // todo => insted of being dependent on the side efect we should be dipendent on the actual event
   // const set = useSelector((state) => state.gameInfo.set);
-  useEffect(() => {
-    setIsSelected(selectedMeld === card.meldId);
-  }, [card.meldId, selectedMeld]);
 
-  const toggleSelection = () => {
-    setIsSelected(selectedMeld === card.meldId);
-    dispatch(toggle_selected_meld(card.meldId));
-  };
   return (
     <>
-      <div
-        className="box"
-        style={{ backgroundImage: `url(${imageFile})` }}
-        onClick={() => toggleSelection()}
-      >
-        {isSelected && (
-          <IoMdCheckmarkCircleOutline
-            className="Selected_Card"
-            onClick={() => toggleSelection()}
-            style={{ color: "green", opacity: "100%" }}
-          />
-        )}
+      <div className="box" style={{ backgroundImage: `url(${imageFile})` }}>
+        <MeldSelection meldId={card.meldId} />
       </div>
     </>
   );
-}
+}, areEqual);
 
 Meld.propTypes = {
   card: PropTypes.object,
