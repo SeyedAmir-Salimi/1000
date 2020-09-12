54d6a30929531256b040164b0de3a45e2e2bdb9b
SeyedAmir-Salimi <you@example.com>
2020-09-12 18:18:15 +0200

    link params

--- src/API/index.js
+++ src/API/index.js
@@ -1,19 +1,24 @@
 import Axios from "axios";
 
 export const fetchGameInfo = async (gameId) => {
-  const result = await Axios.get(`http://localhost:3000/game/${gameId}`);
+  const result = await Axios.get(
+    `https://rummy-app.netlify.app/game/${gameId}`
+  );
   return result.data;
 };
 
 export const generateHands = async (gameId) => {
-  const result = await Axios.post(`http://localhost:3000/cards/generateHands`, {
-    gameId,
-  });
+  const result = await Axios.post(
+    `https://rummy-app.netlify.app/cards/generateHands`,
+    {
+      gameId,
+    }
+  );
   return result.data;
 };
 
 export const createGame = async (PN, username) => {
-  const result = await Axios.post("http://localhost:3000/game/start", {
+  const result = await Axios.post("https://rummy-app.netlify.app/game/start", {
     playerNumbers: PN,
     username: username,
   });
@@ -21,15 +26,18 @@ export const createGame = async (PN, username) => {
 };
 
 export const discard = async (cardId, gameId) => {
-  const result = await Axios.put(`http://localhost:3000/actions/discard`, {
-    id: cardId,
-    gameId,
-  });
+  const result = await Axios.put(
+    `https://rummy-app.netlify.app/actions/discard`,
+    {
+      id: cardId,
+      gameId,
+    }
+  );
   return result.data;
 };
 
 export const createMeldFromCards = async (ids, meldId, gameId) => {
-  const result = await Axios.put(`http://localhost:3000/actions/meld`, {
+  const result = await Axios.put(`https://rummy-app.netlify.app/actions/meld`, {
     cardIds: ids,
     userId: "User4",
     gameId,
